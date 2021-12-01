ba2ce9df9f9e15c21bfc265c28f60370f8988c54
Johan Steen <johan.steen@boost.ai>
2021-12-01 12:34:19 +0100

    Day 1

---
+++ johan/1.py
@@ -0,0 +1,26 @@
+with open("input.1") as f:
+    depths = f.readlines()
+    depths = [int(depth.strip()) for depth in depths]
+
+
+def number_of_increases(depths):
+    prev = None
+    increases = 0
+
+    for depth in depths:
+        if prev is not None and depth > prev:
+            increases += 1
+        prev = depth
+    
+    return increases
+
+
+window_sum = []
+for i in range(len(depths)):
+    window = depths[i: i + 3]
+    if len(window) == 3:
+        window_sum.append(sum(window))
+
+
+print(number_of_increases(depths))
+print(number_of_increases(window_sum))
