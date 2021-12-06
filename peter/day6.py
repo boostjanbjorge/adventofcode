51e91ac2ea168ac13ad60f5cdb2bbf302fd5617d
peterboost <peter@boost.ai>
2021-12-06 12:56:57 +0100

    day6

---
+++ peter/day6.py
@@ -0,0 +1,20 @@
+import collections
+import tqdm
+import copy
+
+fish = open("inputs/day6.txt").read().strip().split(",")
+fish = [int(f) for f in fish]
+
+fish = collections.Counter(fish)
+
+for day in tqdm.tqdm(range(256)):
+    next_fish = collections.Counter()
+    for days, count in fish.items():
+        if days == 0:
+            next_fish[6] += count
+            next_fish[8] += count
+        else:
+            next_fish[days - 1] += count
+    fish = next_fish
+
+print(sum(fish.values()))
