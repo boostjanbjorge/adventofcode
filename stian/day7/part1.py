17ecf58d0c8ed284cfe36b9e00368601a41d47f4
Stian Lode <stian.lode@boost.ai>
2021-12-09 14:09:11 +0100

    Day7: Stian. optimized part1.py

--- stian/day7/part1.py
+++ stian/day7/part1.py
@@ -1,12 +1,11 @@
 import numpy as np; from collections import Counter
 crab_dist = Counter(np.fromfile("day7.txt", dtype=int, sep=","))
 upper = max(crab_dist.keys())
-cost = np.zeros((upper,), dtype=int)
-for k,v in crab_dist.items():
-    for o, i in enumerate(range(k-1, upper)):
-        cost[i] += v*o
-    for o, i in enumerate(range(k-1, -1, -1)):
-        cost[i] += v*o
-print(np.argmin(cost)+1, np.min(cost))
+base = np.zeros((upper+1,), dtype=int)
+for k, v in crab_dist.items(): base[k] = v
+fw = np.cumsum(np.cumsum(np.roll(base, 1)))
+bw = np.cumsum(np.cumsum(np.roll(base[::-1], -1)))
+cost = fw+bw[::-1]
+print(np.argmin(cost), np.min(cost))
 
 
