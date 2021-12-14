29edfdd8838eb19f5abc5a597a16d72d7a35771b
peterboost <peter@boost.ai>
2021-12-14 18:42:56 +0100

    counter

--- peter/day14.py
+++ peter/day14.py
@@ -5,16 +5,17 @@ with open("inputs/day14.txt") as f:
     lines = f.read().strip().split("\n")
 
 init = lines[0]
-pair_counts = collections.OrderedDict()
+pair_counts = collections.Counter()
 for c1, c2 in zip(init[:-1], init[1:]):
     pair_counts[c1 + c2] = pair_counts.get(c1 + c2, 0) + 1
+
 maps = [l.split(" -> ") for l in lines[2:]]
 maps = {
     m[0]: m[1] for m in maps
 }
 
 for _ in range(40):
-    new_pairs = collections.OrderedDict()
+    new_pairs = collections.Counter()
     char_count = collections.Counter()
     for i, (pair, count) in enumerate(pair_counts.items()):
         triplet = pair[0] + maps[pair] + pair[1]
@@ -23,12 +24,12 @@ for _ in range(40):
         char_count[triplet[1]] += count
         char_count[pair[1]] += count / 2
 
-        new_pairs[triplet[:-1]] = new_pairs.get(triplet[:-1], 0) + count
-        new_pairs[triplet[1:]] = new_pairs.get(triplet[1:], 0) + count
+        new_pairs[triplet[:-1]] += count
+        new_pairs[triplet[1:]] += count
 
     pair_counts = new_pairs
 
 char_count[init[0]] += 0.5
 char_count[init[-1]] += 0.5
 
-print(char_count.most_common()[0][1] - char_count.most_common()[-1][1])
+print(int(char_count.most_common()[0][1] - char_count.most_common()[-1][1]))
