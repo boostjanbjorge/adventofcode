32e5b88874a0c0a4e2130b69e20c325cc5005d8a
peterboost <peter@boost.ai>
2021-12-14 18:39:54 +0100

    partw

--- peter/day14.py
+++ peter/day14.py
@@ -1,24 +1,34 @@
 import collections
-import tqdm
+
 
 with open("inputs/day14.txt") as f:
     lines = f.read().strip().split("\n")
 
-init = list(lines[0])
+init = lines[0]
+pair_counts = collections.OrderedDict()
+for c1, c2 in zip(init[:-1], init[1:]):
+    pair_counts[c1 + c2] = pair_counts.get(c1 + c2, 0) + 1
 maps = [l.split(" -> ") for l in lines[2:]]
 maps = {
     m[0]: m[1] for m in maps
 }
 
-# part 1
-for _ in tqdm.tqdm(range(10)):
-    seq = []
-    for i, (c1, c2) in enumerate(zip(init[:-1], init[1:])):
-        seq.append(c1)
-        seq.append(maps[c1 + c2])
-        if i == len(init) - 2:
-            seq.append(c2)
-    init = seq
+for _ in range(40):
+    new_pairs = collections.OrderedDict()
+    char_count = collections.Counter()
+    for i, (pair, count) in enumerate(pair_counts.items()):
+        triplet = pair[0] + maps[pair] + pair[1]
 
-counts = collections.Counter(seq)
-print(counts.most_common()[0][1] - counts.most_common()[-1][1])
+        char_count[pair[0]] += count / 2
+        char_count[triplet[1]] += count
+        char_count[pair[1]] += count / 2
+
+        new_pairs[triplet[:-1]] = new_pairs.get(triplet[:-1], 0) + count
+        new_pairs[triplet[1:]] = new_pairs.get(triplet[1:], 0) + count
+
+    pair_counts = new_pairs
+
+char_count[init[0]] += 0.5
+char_count[init[-1]] += 0.5
+
+print(char_count.most_common()[0][1] - char_count.most_common()[-1][1])
