19621505769f3880b350c9c5545ece0d28f5b2b9
Jan Bjørge (mac) <janbjorge@gmail.com>
2021-12-15 23:08:18 +0100

    Day 15 part 2

--- JB/15.py
+++ JB/15.py
@@ -2,7 +2,7 @@ import queue
 
 
 def load():
-    gph = {}
+    gph: dict[tuple[int, int], int] = {}
     with open("inputs/15.txt") as f:
         for y, row in enumerate(f.readlines()):
             for x, risk in enumerate(row):
@@ -11,13 +11,12 @@ def load():
     return gph
 
 
-def walk(
-    start: tuple[int, int],
-    stop: tuple[int, int],
-    gph: dict[tuple[int, int], int],
-):
+def top_left_to_bottom_left(gph: dict[tuple[int, int], int]):
     minx, maxx = min(x for x, _ in gph), max(x for x, _ in gph)
     miny, maxy = min(y for _, y in gph), max(y for _, y in gph)
+
+    start = (minx, miny)
+    stop = (maxx, maxy)
 
     pq: queue.PriorityQueue[tuple[int, tuple[int, int]]] = queue.PriorityQueue()
     pq.put((0, start))
@@ -31,12 +30,31 @@ def walk(
 
         # Neighbors
         for x, y in ((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)):
-            p = (x, y)
-            if minx <= x <= maxx and miny <= y <= maxy and p not in visited:
-                pq.put((acc_risk + gph[p], p))
-                visited.add(p)
+            if minx <= x <= maxx and miny <= y <= maxy and (x, y) not in visited:
+                pq.put((acc_risk + gph[(x, y)], (x, y)))
+                visited.add((x, y))
 
     raise ValueError("No path")
 
 
-print(walk((0, 0), (99, 99), load()))
+def extended(rx=5, ry=5):
+    gph = load()
+    maxx = max(x for x, _ in gph) + 1
+    maxy = max(y for _, y in gph) + 1
+
+    for x, y in tuple(gph.keys()):
+        for sx in range(rx):
+            for sy in range(ry):
+                gph[(x + sx * maxx, y + sy * maxy)] = gph[(x, y)] + sx + sy
+
+    for key, risk in tuple(gph.items()):
+        while risk > 9:
+            risk -= 9
+        gph[key] = risk
+
+    return gph
+
+
+if __name__ == "__main__":
+    print("a:", top_left_to_bottom_left(load()))
+    print("b:", top_left_to_bottom_left(extended()))
